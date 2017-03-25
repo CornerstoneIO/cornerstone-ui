ae704e227fb2b4b24c889e83a1125addd267f6ea
Scott May <scottmay@Scotts-MacBook-Pro.local>
2017-03-25 18:50:18 -0400

    set up some routes

--- app/router.js
+++ app/router.js
@@ -7,6 +7,12 @@ const Router = Ember.Router.extend({
 });
 
 Router.map(function() {
+  this.route('login');
+  this.route('dashboard');
+  this.route('organization', function() {
+    this.route('church-info');
+    this.route('locations');
+  });
 });
 
 export default Router;
