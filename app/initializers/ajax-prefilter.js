9ff7b315dceb36d8b2063e8395db96ba91b47d69
Scott May <scottmay@Scotts-MacBook-Pro.local>
2017-04-25 21:22:37 -0400

    Just getting rid of some eslint errors

--- app/initializers/ajax-prefilter.js
+++ app/initializers/ajax-prefilter.js
@@ -1,4 +1,7 @@
-export function initialize(container, application) {
+import Ember from 'ember';
+import Cookies from 'js-cookie';
+
+export function initialize(/*application*/) {
   Ember.$.ajaxPrefilter(function(options, originalOptions, jqXHR) {
     var token = Cookies.get('authenticationToken');
     if(token) {
